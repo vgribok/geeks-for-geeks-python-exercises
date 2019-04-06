4e3ad1b6bd0315b31f8d5ee8fe0fe72734114872
Vlad Hrybok <vgribok@hotmail.com>
2019-04-06 16:16:36 -0400

    Done with borrowed heapsort

--- main.py
+++ main.py
@@ -10,43 +10,43 @@ def main():
         nAndK = readIntArray()
         k = nAndK[1]
         numArray = readIntArray()
-        largestNumbers = bubbleSortDescending(numArray, k)
-        printArray(largestNumbers, k)
+        heapsort(numArray)
+        printArray(numArray, k)
 
 def readIntArray():
     stringInputArray = sys.stdin.readline().split()
     return list(map(int, stringInputArray))
 
 def printArray(anArray, maxLen):
+
+    arrayLen = len(anArray)
+
     i=0
-    for elem in anArray:
-        if i >= maxLen:
-            break
-        print(elem, end =" ") 
+    while i < maxLen:
+        print(anArray[arrayLen - 1 - i], end =" ") 
         i = i+1
     print()
 
-def bubbleLargerToTheLeft(numArray, arrayLen, stopIndex):
-    
-    i = arrayLen-1
+# Heap Sort is lifted from https://stackoverflow.com/a/14011249/516508
+def heapsort(s):                               
+    sl = len(s)                                    
 
-    while i > stopIndex:
-        right = numArray[i]
-        left = numArray[i-1]
-        if right > left:
-            numArray[i-1] = right
-            numArray[i] = left
-        i = i-1
+    def swap(pi, ci):                              
+        if s[pi] < s[ci]:                          
+            s[pi], s[ci] = s[ci], s[pi]            
 
-def bubbleSortDescending(numArray, limit):
-
-    arrayLen = len(numArray)
-    count = 0
-    
-    while count < limit:
-        bubbleLargerToTheLeft(numArray, arrayLen, count)
-        count = count + 1
-
-    return numArray
+    def sift(pi, unsorted):                        
+        i_gt = lambda a, b: a if s[a] > s[b] else b
+        while pi*2+2 < unsorted:                   
+            gtci = i_gt(pi*2+1, pi*2+2)            
+            swap(pi, gtci)                         
+            pi = gtci                              
+    # heapify                                      
+    for i in range((sl >> 1) - 1, -1, -1):              
+        sift(i, sl)                                
+    # sort                                         
+    for i in range(sl-1, 0, -1):                   
+        swap(i, 0)                                 
+        sift(0, i)    
 
 main()
